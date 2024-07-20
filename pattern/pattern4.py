a6752210f48f5d321401443fd1544a3c2abf8354
suraj <surajankola9@gmail.com>
2024-07-20 08:43:05 +0530

    pattern 4

---
+++ pattern/pattern4.py
@@ -0,0 +1,20 @@
+size=5
+for i in range(size):
+    for j in range(i+1):
+        print(j+1,end=" ")
+    print()
+    
+
+size=5
+for i in range(size):
+    for j in range(i+1):
+        print(size-j,end=" ")
+    print()
+    
+size=5
+count=1
+for i in range(size):
+    for j in range(i+1):
+        print(count,end=" ")
+    print()
+    count+=1
